40515c6f964b245e857aa2cd5daf32362e10e144
Erkka Rinne <erkka.rinne@vtt.fi>
2019-11-21 14:17:47 +0200

    Add long description to the setup script
    Also classified as production/stable.

--- setup.py
+++ setup.py
@@ -6,12 +6,17 @@ from setuptools import setup, find_packages
 with open('gdx2py/version.py') as f:
     exec(f.read())
 
+with open("README.md", "r") as f:
+    readme = f.read()
+
 setup(
     name='GDX2py',
     version=__version__,  # pylint: disable=undefined-variable
     author='Erkka Rinne',
     author_email='erkka.rinne@vtt.fi',
     description='Read and write GAMS Data eXchange (GDX) files using Python',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     python_requires='>=3.6',
     install_requires=['gdxcc>=7'],
     setup_requires=['pytest-runner'],
@@ -24,7 +29,7 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering",
         "Intended Audience :: Developers",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
     ],
     license="MIT",
 )
