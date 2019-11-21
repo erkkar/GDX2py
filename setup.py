b4236765c3e0f92ab459c1521d29a0d99e48f90c
Erkka Rinne <erkka.rinne@vtt.fi>
2019-11-21 14:17:47 +0200

    Add long description to the setup script
    Also classified as production/stable.
    
    (cherry picked from commit 40515c6f964b245e857aa2cd5daf32362e10e144)

--- setup.py
+++ setup.py
@@ -3,27 +3,32 @@
 from setuptools import setup, find_packages
 
 # Get version string
-with open('gdx2py/version.py') as f: exec(f.read())
+with open('gdx2py/version.py') as f:
+    exec(f.read())
 
-setup(name='GDX2py',
-      version=__version__,  # pylint: disable=undefined-variable
-      author='Erkka Rinne',
-      author_email='erkka.rinne@vtt.fi',
-      description='Read and write GAMS Data eXchange (GDX) files using Python',
-      python_requires='>=3.6',
-      install_requires=[
-        'gdxcc>=7',
-      ],
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest', 'pytest-datadir'],
-      url='https://github.com/ererkka/GDX2py',
-      packages=find_packages(exclude=['tests']),
-      classifiers=[
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name='GDX2py',
+    version=__version__,  # pylint: disable=undefined-variable
+    author='Erkka Rinne',
+    author_email='erkka.rinne@vtt.fi',
+    description='Read and write GAMS Data eXchange (GDX) files using Python',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+    install_requires=['gdxcc>=7'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-datadir'],
+    url='https://github.com/ererkka/GDX2py',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering",
         "Intended Audience :: Developers",
-        "Development Status :: 4 - Beta"
+        "Development Status :: 5 - Production/Stable",
     ],
 )
