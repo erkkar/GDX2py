73bca7ab2ebddfebaf25a5ac10adb1d9fccc4384
Erkka Rinne <erkka.rinne@vtt.fi>
2019-11-25 12:16:29 +0200

    Add support for Google style docstrings
    Using Sphinx extension Napoleon for this.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['autoapi.extension']
+extensions = ['sphinx.ext.napoleon', 'autoapi.extension']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
