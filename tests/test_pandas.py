7d5cd0af8ae7173a8d3b3fd0972452fd96e60a91
Erkka Rinne <erkka.rinne@vtt.fi>
2020-11-22 14:27:00 +0200

    Add a test for writing Pandas series to file

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 
+from gdx2py.gdxfile import GdxFile
 from gdx2py.gams import GAMSParameter
 from .constants import PAR1, PAR2, PAR1_TEXT
 
@@ -25,3 +26,8 @@ class TestPandas:
         par = GAMSParameter(PAR1, expl_text=PAR1_TEXT)
         assert par.to_pandas().name == PAR1_TEXT
 
+    def test_write_series(self, tmp_path):
+        filepath = tmp_path / 'test.gdx'
+        ts = pd.Series(PAR1)
+        with GdxFile(filepath, mode='w') as gdx:
+            gdx['ts'] = GAMSParameter(ts)
